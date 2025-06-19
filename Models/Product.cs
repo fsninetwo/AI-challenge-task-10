2441511ef9b218d07fc269ed4334c333ee705909
Mikita Miadzvedzeu <mikita.miadzvedzeu@ventionteams.com>
2025-06-19 12:41:03 +0300

    apply serialization

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,20 @@
 namespace AIConsoleApp.Models;
+using System.Text.Json.Serialization;
 
 public class Product
 {
+    [JsonPropertyName("name")] 
     public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("category")] 
     public string Category { get; set; } = string.Empty;
+
+    [JsonPropertyName("price")] 
     public decimal Price { get; set; }
+
+    [JsonPropertyName("rating")] 
     public double Rating { get; set; }
+
+    [JsonPropertyName("in_stock")]
     public bool InStock { get; set; }
 } 
